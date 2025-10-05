4be00fa109f86cf8e366a7045b80d6a900b77c3c
Marcio <marcio28@gmail.com>
2025-10-05 10:10:02 -0300

    Criação de entidade Curso e de teste de matrícula em curso indisponível.

--- tests/EducacaoOnline.GestaoDeAlunos.Domain.Tests/AlunoTests.cs
+++ tests/EducacaoOnline.GestaoDeAlunos.Domain.Tests/AlunoTests.cs
@@ -1,4 +1,6 @@
 ﻿
+using EducacaoOnline.Core.DomainObjects;
+
 namespace EducacaoOnline.GestaoDeAlunos.Domain.Tests
 {
     public class AlunoTests
@@ -9,19 +11,31 @@ namespace EducacaoOnline.GestaoDeAlunos.Domain.Tests
         {
             // Arrange
             var aluno = new Aluno();
-            var idCursoSelecionado = Guid.NewGuid();
+            var cursoSelecionado = new Curso(disponivelMatricula: true);
             var quatidadeMatriculasAntes = aluno.Matriculas.Count;
 
             // Act
-            var matricula = aluno.IniciarMatricula(idCurso: idCursoSelecionado);
+            var matricula = aluno.IniciarMatricula(cursoSelecionado);
 
             // Assert
             Assert.Equal(0, aluno.ValidationResult?.Errors.Count ?? 0);
             var quatidadeMatriculasDepois = aluno.Matriculas.Count;
             Assert.Equal(quatidadeMatriculasAntes + 1, quatidadeMatriculasDepois);
             Assert.Equal(aluno.Id, matricula.IdAluno);
-            Assert.Equal(idCursoSelecionado, matricula.IdCurso);
+            Assert.Equal(cursoSelecionado.Id, matricula.IdCurso);
             Assert.Equal(StatusMatricula.AguardandoPagamento, matricula.Status);
+        }
+
+        [Fact(DisplayName = "Iniciar Matrícula Indisponível Lança Exceção")]
+        [Trait("Categoria", "Gestão de Alunos - Aluno")]
+        public void IniciarMatricula_Indisponivel_LancaExcecao()
+        {
+            // Arrange
+            var aluno = new Aluno();
+            var cursoSelecionado = new Curso(disponivelMatricula: false);
+
+            // Act && Assert
+            var excecao = Assert.Throws<DomainException>(() => aluno.IniciarMatricula(cursoSelecionado));
         }
     }
 }
