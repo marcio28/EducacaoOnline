680e75ce72ae6d07ceada67e3e091ee976573054
Marcio <marcio28@gmail.com>
2025-10-05 10:49:05 -0300

    Novo método.

--- src/EducacaoOnline.Core/DomainObjects/DomainException.cs
+++ src/EducacaoOnline.Core/DomainObjects/DomainException.cs
@@ -7,19 +7,23 @@ namespace EducacaoOnline.Core.DomainObjects
     {
         public string[] RegrasVioladas = [];
 
-        public DomainException()
-        {
-        }
+        public DomainException() { }
 
-        public DomainException(string? message) : base(message)
-        {
-        }
+        public DomainException(string? message) : base(message) { }
 
-        public DomainException(string? message, Exception? innerException) : base(message, innerException)
+        public DomainException(string? message, Exception? innerException) : base(message, innerException) { }
+
+        public DomainException(List<ValidationFailure>? validationFailures) : base()
         {
+            ExtrairRegrasVioladas(validationFailures);
         }
 
         public DomainException(string? message, List<ValidationFailure>? validationFailures) : base(message)
+        {
+            ExtrairRegrasVioladas(validationFailures);
+        }
+
+        private void ExtrairRegrasVioladas(List<ValidationFailure>? validationFailures)
         {
             if (validationFailures is not null)
                 RegrasVioladas = [.. validationFailures.Select(vf => vf.ErrorMessage)];
