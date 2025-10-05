63174e87ad57cd544ca9aee9af0100a0c3f96425
Marcio <marcio28@gmail.com>
2025-10-05 10:51:36 -0300

    Implementada AulaInvalidaException.

---
+++ src/EducacaoOnline.GestaoDeConteudo.Domain/Exceptions/AulaInvalidaException.cs
@@ -0,0 +1,11 @@
+﻿
+using EducacaoOnline.Core.DomainObjects;
+using FluentValidation.Results;
+
+namespace EducacaoOnline.GestaoDeConteudo.Domain.Exceptions
+{
+    public class AulaInvalidaException : DomainException
+    {
+        public AulaInvalidaException(List<ValidationFailure>? validationFailures) : base("Aula inválida.", validationFailures) { }
+    }
+}
