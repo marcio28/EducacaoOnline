00026d98ba6244df2929af5ee2dc3645f8fec6a3
Marcio <marcio28@gmail.com>
2025-10-05 10:50:13 -0300

    Implementada MatriculaCursoIndisponivelException.

---
+++ src/EducacaoOnline.GestaoDeAlunos.Domain/Exceptions/MatriculaCursoIndisponivelException.cs
@@ -0,0 +1,13 @@
+﻿using EducacaoOnline.Core.DomainObjects;
+
+namespace EducacaoOnline.GestaoDeAlunos.Domain.Exceptions
+{
+    public class MatriculaCursoIndisponivelException : DomainException
+    {
+        public MatriculaCursoIndisponivelException() : base("Curso indisponível para matrícula.") { }
+
+        public MatriculaCursoIndisponivelException(string? message) : base(message) { }
+
+        public MatriculaCursoIndisponivelException(string? message, Exception? innerException) : base(message, innerException) { }
+    }
+}
