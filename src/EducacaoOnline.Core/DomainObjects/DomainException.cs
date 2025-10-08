e784cd6b83f400fa933566b9a6e04fb4db8e9e76
Marcio <marcio28@gmail.com>
2025-10-08 00:25:49 -0300

    Simplificação de DomainException.

--- src/EducacaoOnline.Core/DomainObjects/DomainException.cs
+++ src/EducacaoOnline.Core/DomainObjects/DomainException.cs
@@ -5,28 +5,8 @@ namespace EducacaoOnline.Core.DomainObjects
     [Serializable]
     public class DomainException : Exception
     {
-        public string[] RegrasVioladas = [];
-
-        public DomainException() { }
-
         public DomainException(string? message) : base(message) { }
 
         public DomainException(string? message, Exception? innerException) : base(message, innerException) { }
-
-        public DomainException(List<ValidationFailure>? validationFailures) : base()
-        {
-            ExtrairRegrasVioladas(validationFailures);
-        }
-
-        public DomainException(string? message, List<ValidationFailure>? validationFailures) : base(message)
-        {
-            ExtrairRegrasVioladas(validationFailures);
-        }
-
-        private void ExtrairRegrasVioladas(List<ValidationFailure>? validationFailures)
-        {
-            if (validationFailures is not null)
-                RegrasVioladas = [.. validationFailures.Select(vf => vf.ErrorMessage)];
-        }
     }
 }
