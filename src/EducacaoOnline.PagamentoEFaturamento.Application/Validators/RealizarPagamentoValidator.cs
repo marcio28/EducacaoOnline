412d25be7d26dc64becac8e0490412748ed5b682
Marcio <marcio28@gmail.com>
2025-10-12 10:33:14 -0300

    Criado validador de RealizarPagamentoCommand.

---
+++ src/EducacaoOnline.PagamentoEFaturamento.Application/Validators/RealizarPagamentoValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace EducacaoOnline.PagamentoEFaturamento.Application.Validators
+{
+    public class RealizarPagamentoValidator : AbstractValidator<RealizarPagamentoCommand>
+    {
+        public static string IdMatriculaErroMsg => "Id da matrícula inválido.";
+        public static string DadosCartaoErroMsg => "Dados do cartão inválidos.";
+
+        public RealizarPagamentoValidator()
+        {
+            RuleFor(c => c.IdMatricula)
+                .NotEqual(Guid.Empty)
+                .WithMessage(IdMatriculaErroMsg);
+
+            RuleFor(c => c.ErrosCartao.Count)
+                .Equal(0)
+                .WithMessage(DadosCartaoErroMsg);
+        }
+    }
+}
