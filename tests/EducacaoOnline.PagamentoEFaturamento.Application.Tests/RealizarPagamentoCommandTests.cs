e107fb7719f961171683905ee86e947eee024946
Marcio <marcio28@gmail.com>
2025-10-12 10:38:20 -0300

    Implementado mais um teste.

--- tests/EducacaoOnline.PagamentoEFaturamento.Application.Tests/RealizarPagamentoCommandTests.cs
+++ tests/EducacaoOnline.PagamentoEFaturamento.Application.Tests/RealizarPagamentoCommandTests.cs
@@ -1,16 +1,17 @@
+using EducacaoOnline.PagamentoEFaturamento.Application.Validators;
 using EducacaoOnline.PagamentoEFaturamento.Domain;
 
 namespace EducacaoOnline.PagamentoEFaturamento.Application.Tests
 {
     public class RealizarPagamentoCommandTests
     {
-        [Fact(DisplayName = "Realizar Pagamento Sem Violações É Válido")]
+        [Fact(DisplayName = "Realizar Pagamento Sem Erros É Válido")]
         [Trait("Categoria", "Pagamento e Faturamento - Pagamento Commands")]
-        public void RealizarPagamentoCommand_SemViolacoes_EhValido()
+        public void RealizarPagamentoCommand_SemErros_EhValido()
         {
             // Arrange
             var dadosCartao = new DadosCartao(nomeTitular: "Nome Teste",
-                                              numeroCartao: "1234567890123456",
+                                              numeroCartao: "4024007164015884",
                                               codigoSeguranca: "123",
                                               dataValidade: DateTime.Now.AddYears(1));
             
@@ -20,6 +21,27 @@ namespace EducacaoOnline.PagamentoEFaturamento.Application.Tests
 
             // Assert
             Assert.True(realizarPagamentoCommand.EhValido());
+        }
+
+        [Fact(DisplayName = "Realizar Pagamento Com Erros É Inválido")]
+        [Trait("Categoria", "Pagamento e Faturamento - Pagamento Commands")]
+        public void RealizarPagamentoCommand_ComErros_EhInvalido()
+        {
+            // Arrange
+            var dadosCartao = new DadosCartao(nomeTitular: "",
+                                              numeroCartao: "",
+                                              codigoSeguranca: "",
+                                              dataValidade: DateTime.Now.AddDays(-1));
+
+            // Act
+            var realizarPagamentoCommand = new RealizarPagamentoCommand(idMatricula: Guid.Empty,
+                                                                        dadosCartao: dadosCartao);
+
+            // Assert
+            Assert.False(realizarPagamentoCommand.EhValido());
+            Assert.Equal(2, realizarPagamentoCommand.QuantidadeErros);
+            Assert.Contains(RealizarPagamentoValidator.IdMatriculaErroMsg, realizarPagamentoCommand.Erros.Select(c => c.ErrorMessage));
+            Assert.Contains(RealizarPagamentoValidator.DadosCartaoErroMsg, realizarPagamentoCommand.Erros.Select(c => c.ErrorMessage));
         }
     }
 }
