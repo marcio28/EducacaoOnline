973cc24c609f38015f637c582ce8fe642931a4c9
Marcio <marcio28@gmail.com>
2025-10-12 10:30:36 -0300

    Criada classe abstrata ValueObject.

---
+++ src/EducacaoOnline.Core/DomainObjects/ValueObject.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation.Results;
+
+namespace EducacaoOnline.Core.DomainObjects
+{
+    public abstract class ValueObject
+    {
+        public ValidationResult? ValidationResult { get; protected set; }
+        public List<ValidationFailure> Erros => ValidationResult?.Errors ?? [];
+        public int QuantidadeErros => Erros.Count;
+
+        public virtual bool EhValido()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
