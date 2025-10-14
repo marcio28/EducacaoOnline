bdc6d185af0ae5c80238c744bc408fb386a34f02
Marcio <marcio28@gmail.com>
2025-10-14 00:24:50 -0300

    Adicionada lista de eventos em Entity.

--- src/EducacaoOnline.Core/DomainObjects/Entity.cs
+++ src/EducacaoOnline.Core/DomainObjects/Entity.cs
@@ -1,14 +1,70 @@
-﻿using FluentValidation.Results;
+﻿using EducacaoOnline.Core.Messages;
+using FluentValidation.Results;
 
 namespace EducacaoOnline.Core.DomainObjects
 {
     public abstract class Entity
     {
-        public Guid Id { get; private set; } = Guid.NewGuid();
-
+        public Guid Id { get; }
+        List<Event>? _notificacoes;
+        public IReadOnlyCollection<Event> Notificacoes => (_notificacoes ?? []).AsReadOnly();
         public ValidationResult? ValidationResult { get; protected set; }
         public List<ValidationFailure> Erros => ValidationResult?.Errors ?? [];
         public int QuantidadeErros => Erros.Count;
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public void AdicionarEvento(Event evento)
+        {
+            _notificacoes = _notificacoes ?? [];
+            _notificacoes.Add(evento);
+        }
+
+        public void RemoverEvento(Event evento)
+        {
+            _notificacoes?.Remove(evento);
+        }
+
+        public void LimparEventos()
+        {
+            _notificacoes?.Clear();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            var compareTo = obj as Entity;
+
+            if (ReferenceEquals(this, compareTo)) return true;
+            if (compareTo is null) return false;
+
+            return Id.Equals(compareTo.Id);
+        }
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (a is null && b is null) return true;
+            if (a is null || b is null) return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().GetHashCode() * 907) + Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} [Id={Id}]";
+        }
 
         public virtual bool EhValido()
         {
