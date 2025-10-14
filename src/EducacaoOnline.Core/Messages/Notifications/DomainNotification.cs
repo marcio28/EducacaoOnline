cedda1b25000704d75f904a101f3178831e67b32
Marcio <marcio28@gmail.com>
2025-10-14 00:26:03 -0300

    Adicionadas DomainNotification e Handler.

---
+++ src/EducacaoOnline.Core/Messages/Notifications/DomainNotification.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+
+namespace EducacaoOnline.Core.Messages.Notifications
+{
+    public class DomainNotification : Message, INotification
+    {
+        public Guid Id { get; }
+        public DateTime DataHora { get; }
+        public string Chave { get; }
+        public string Valor { get; }
+        public int Versao { get; }
+
+        public DomainNotification(string chave, string valor)
+        {
+            Id = Guid.NewGuid();
+            DataHora = DateTime.Now;
+            Chave = chave;
+            Valor = valor;
+            Versao = 1;
+        }
+    }
+}
---
+++ src/EducacaoOnline.Core/Messages/Notifications/DomainNotificationHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+
+namespace EducacaoOnline.Core.Messages.Notifications
+{
+    public class DomainNotificationHandler : INotificationHandler<DomainNotification>
+    {
+        List<DomainNotification> _notificacoes;
+
+        public DomainNotificationHandler()
+        {
+            _notificacoes = [];
+        }
+
+        public Task Handle(DomainNotification message, CancellationToken cancellationToken)
+        {
+            _notificacoes.Add(message);
+            return Task.CompletedTask;
+        }
+
+        public virtual List<DomainNotification> ObterNotificacoes()
+        {
+            return _notificacoes;
+        }
+
+        public virtual bool TemNotificacoes()
+        {
+            return ObterNotificacoes().Count != 0;
+        }
+
+        public void Dispose()
+        {
+            _notificacoes = [];
+        }
+
+    }
+}
