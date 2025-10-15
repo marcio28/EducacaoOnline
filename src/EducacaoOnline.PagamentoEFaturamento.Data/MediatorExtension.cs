6dba1704603d822138cbb1e4ffd8802047296e3f
Marcio <marcio28@gmail.com>
2025-10-14 23:41:30 -0300

    Criação do projeto PagamentoEFaturamento.Data.

---
+++ src/EducacaoOnline.PagamentoEFaturamento.Data/MediatorExtension.cs
@@ -0,0 +1,28 @@
+﻿using EducacaoOnline.Core.DomainObjects;
+using EducacaoOnline.PagamentoEFaturamento.Data;
+using MediatR;
+
+public static class MediatorExtension
+{
+    public static async Task PublicarEventos(this IMediator mediator, PagamentoEFaturamentoContext context)
+    {
+        var entityEntries = context.ChangeTracker
+            .Entries<Entity>()
+            .Where(e => e.Entity.Notificacoes is not null && e.Entity.Notificacoes.Count != 0);
+
+        var comEventosEntityEntries = entityEntries
+            .SelectMany(x => x.Entity.Notificacoes)
+            .ToList();
+
+        entityEntries
+            .ToList()
+            .ForEach(entityEntry => entityEntry.Entity.LimparEventos());
+
+        var tasks = comEventosEntityEntries
+            .Select(async (eventoDeDominio) => {
+                await mediator.Publish(eventoDeDominio);
+            });
+
+        await Task.WhenAll(tasks);
+    }
+}
---
+++ src/EducacaoOnline.PagamentoEFaturamento.Data/PagamentoEFaturamentoContext.cs
@@ -0,0 +1,25 @@
+﻿using EducacaoOnline.Core.Data;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace EducacaoOnline.PagamentoEFaturamento.Data
+{
+    public class PagamentoEFaturamentoContext : DbContext, IUnitOfWork
+    {
+        private readonly IMediator _mediator;
+
+        public PagamentoEFaturamentoContext(DbContextOptions<PagamentoEFaturamentoContext> options,
+                                            IMediator mediator) : base(options)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<bool> Commit()
+        {
+            var sucesso = await base.SaveChangesAsync() > 0;
+            if (sucesso) await _mediator.PublicarEventos(this);
+
+            return sucesso;
+        }
+    }
+}
