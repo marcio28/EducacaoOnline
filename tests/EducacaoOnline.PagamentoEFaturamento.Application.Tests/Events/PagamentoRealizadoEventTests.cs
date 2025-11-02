e714397e28ebc6dfe60cc760c881992b3ddb2cea
Marcio <marcio28@gmail.com>
2025-11-02 01:16:24 -0300

    Teste PagamentoRealizadoEvent_Propriedades_DevemEstarPreenchid implementado.

---
+++ tests/EducacaoOnline.PagamentoEFaturamento.Application.Tests/Events/PagamentoRealizadoEventTests.cs
@@ -0,0 +1,23 @@
+﻿using EducacaoOnline.PagamentoEFaturamento.Application.Events;
+
+namespace EducacaoOnline.PagamentoEFaturamento.Application.Tests.Events
+{
+    public class PagamentoRealizadoEventTests
+    {
+        [Fact]
+        [Trait("Categoria", "Pagamento e Faturamento - PagamentoRealizadoEvent")]
+        public void PagamentoRealizadoEvent_Propriedades_DevemEstarPreenchidas()
+        {
+            // Arrange
+            var idPagamento = Guid.NewGuid();
+            var idMatricula = Guid.NewGuid();
+
+            // Act
+            var pagamentoRealizadoEvent = new PagamentoRealizadoEvent(idPagamento, idMatricula);
+
+            // Assert
+            Assert.Equal(idPagamento, pagamentoRealizadoEvent.IdPagamento);
+            Assert.Equal(idMatricula, pagamentoRealizadoEvent.IdMatricula);
+        }
+    }
+}
