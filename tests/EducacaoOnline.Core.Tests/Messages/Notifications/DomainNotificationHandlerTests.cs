765444ba695799942453961e347e00037d5ec767
Marcio <marcio28@gmail.com>
2025-11-02 01:13:22 -0300

    Teste DomainNotificationHandler_Handle_DeveAdicionarNotificacao implementado.

--- tests/EducacaoOnline.Core.Tests/Messages/Notifications/DomainNotificationHandlerTests.cs
+++ tests/EducacaoOnline.Core.Tests/Messages/Notifications/DomainNotificationHandlerTests.cs
@@ -16,5 +16,25 @@ namespace EducacaoOnline.Core.Tests.Messages.Notifications
             Assert.Empty(domainNotificationHandler.ObterNotificacoes());
             Assert.False(domainNotificationHandler.TemNotificacoes());
         }
+
+        [Fact(DisplayName = "DomainNotificationHandler Handle Adiciona Notificação")]
+        [Trait("Categoria", "Core Messages - DomainNotificationHandler")]
+        public async Task DomainNotificationHandler_Handle_DeveAdicionarNotificacao()
+        {
+            // Arrange
+            var domainNotificationHandler = new DomainNotificationHandler();
+            var notification = new DomainNotification("CHAVE_TESTE", "VALOR_TESTE");
+
+            // Act
+            await domainNotificationHandler.Handle(notification, CancellationToken.None);
+
+            // Assert
+            var notificacoes = domainNotificationHandler.ObterNotificacoes();
+            Assert.NotNull(notificacoes);
+            Assert.Single(notificacoes);
+            Assert.Contains(notification, notificacoes);
+            Assert.True(domainNotificationHandler.TemNotificacoes());
+        }
+
     }
 }
