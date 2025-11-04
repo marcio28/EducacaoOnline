097d4b5ecdd21dcc4bf78919f973c7bc76ecb0c9
Marcio <marcio28@gmail.com>
2025-11-03 22:19:00 -0300

    Implementada autorização via Claims.

---
+++ src/EducacaoOnline.Api/Extensions/ClaimsAuthorization.cs
@@ -0,0 +1,13 @@
+﻿namespace EducacaoOnline.Api.Extensions
+{
+    public class ClaimsAuthorization
+    {
+        public static bool ValidarClaimsUsuario(HttpContext context, string claimName, string claimValue)
+        {
+            if (context.User.Identity == null) throw new InvalidOperationException();
+
+            return context.User.Identity.IsAuthenticated &&
+                   context.User.Claims.Any(c => c.Type == claimName && c.Value.Split(',').Contains(claimValue));
+        }
+    }
+}
