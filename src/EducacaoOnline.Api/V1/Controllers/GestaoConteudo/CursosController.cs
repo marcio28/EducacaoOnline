36df44bcbcd7328cd9133bb1c1d7c59ef992da8f
Marcio <marcio28@gmail.com>
2025-11-09 20:49:21 -0300

    Implementando teste de integração de CursosController.

---
+++ src/EducacaoOnline.Api/V1/Controllers/GestaoConteudo/CursosController.cs
@@ -0,0 +1,95 @@
+﻿using AutoMapper;
+using EducacaoOnline.Api.Controllers;
+using EducacaoOnline.Api.Extensions;
+using EducacaoOnline.Api.Models;
+using EducacaoOnline.Core.Messages.ApplicationNotifications;
+using EducacaoOnline.GestaoConteudo.Domain;
+using EducacaoOnline.GestaoConteudo.Domain.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace EducacaoOnline.Api.V1.Controllers.GestaoConteudo
+{
+    [Authorize]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/gestao-conteudo/cursos")]
+    public class CursosController : MainController
+    {
+        private readonly ICursoService _cursoService;
+        private readonly IMapper _mapper;
+
+        public CursosController(ICursoService cursoService, 
+                                       INotifiable notifiable,
+                                       IMapper mapper) : base(notifiable)
+        {
+            _cursoService = cursoService;
+            _mapper = mapper;
+        }
+
+        [ClaimsAuthorize("Cursos", "INCLUIR")]
+        [HttpPost]
+        public async Task<ActionResult> IncluirCurso([FromBody] CursoModel cursoModel, CancellationToken tokenDeCancelamento)
+        {
+            if (ModelState.IsValid is false)
+            {
+                return RespostaCustomizada(ModelState);
+            }
+
+            await _cursoService.Incluir(_mapper.Map<Curso>(cursoModel), tokenDeCancelamento);
+
+            return RespostaCustomizada(HttpStatusCode.Created);
+        }
+
+        [ClaimsAuthorize("Cursos", "VISUALIZAR")]
+        [HttpGet]
+        public async Task<ActionResult> ListarCursos(CancellationToken tokenDeCancelamento)
+        {
+            return RespostaCustomizada(HttpStatusCode.OK, _mapper.Map<IEnumerable<CursoModel>>(await _cursoService.Listar(tokenDeCancelamento)));
+        }
+
+        [ClaimsAuthorize("Cursos", "ALTERAR")]
+        [HttpPut("{id:Guid}")]
+        public async Task<IActionResult> AlterarCurso(Guid id, [FromBody] CursoModel cursoModel, CancellationToken tokenDeCancelamento)
+        {
+            if (id != cursoModel.Id)
+            {
+                AdicionarErroProcessamento("O Id informado não é o mesmo que foi passado no corpo da requisição.");
+                return RespostaCustomizada();
+            }
+
+            if (ModelState.IsValid is false)
+            {
+                return RespostaCustomizada(ModelState);
+            }
+
+            await _cursoService.Alterar(_mapper.Map<Curso>(cursoModel), tokenDeCancelamento);
+
+            return RespostaCustomizada(HttpStatusCode.NoContent);
+        }
+
+        [ClaimsAuthorize("Cursos", "VISUALIZAR")]
+        [HttpGet("{id:Guid}")]
+        public async Task<ActionResult> ObterCursoPorId(Guid id, CancellationToken tokenDeCancelamento)
+        {
+            var curso = _mapper.Map<CursoModel>(await _cursoService.ObterPorId(id, tokenDeCancelamento));
+
+            return RespostaCustomizada(HttpStatusCode.OK, curso);
+        }
+
+        [ClaimsAuthorize("Cursos", "EXCLUIR")]
+        [HttpDelete("{id:Guid}")]
+        public async Task<ActionResult> ExcluirCurso(Guid id, CancellationToken tokenDeCancelamento)
+        {
+            if (id == Guid.Empty)
+            {
+                AdicionarErroProcessamento("O id não pode ser vazio.");
+                return RespostaCustomizada();
+            }
+
+            await _cursoService.Excluir(id, tokenDeCancelamento);
+
+            return RespostaCustomizada(HttpStatusCode.NoContent);
+        }
+    }
+}
---
+++ tests/EducacaoOnline.Api.Tests/V1/GestaoConteudo/CursosControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using EducacaoOnline.Api.Models;
+using EducacaoOnline.Api.Tests.Configuration;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http.Json;
+
+namespace EducacaoOnline.Api.Tests.V1.GestaoConteudo
+{
+    [Collection(nameof(IntegrationApiTestsFixtureCollection))]
+    public class CursosControllerTests
+    {
+        private readonly IntegrationTestsFixture<Program> _testsFixture;
+        private const string URIGestaoConteudo = "/api/v1/gestao-conteudo";
+        private const string URICursos = $"{URIGestaoConteudo}/cursos";
+
+        public CursosControllerTests(IntegrationTestsFixture<Program> testsFixture)
+        {
+            _testsFixture = testsFixture;
+        }
+
+        [Fact(DisplayName = "Incluir curso, válido, grava curso")]
+        [Trait("Categoria", "Integração API - Gestão Conteúdo - Cursos")]
+        public async Task IncluirCurso_Valido_DeveGravarCurso()
+        {
+            // Arrange
+            await _testsFixture.FazerLoginAdministrador();
+
+            var cursoModel = new CursoModel
+            {
+                Nome = "Curso de Teste",
+                Descricao = "Descrição do Curso de Teste",
+            };
+
+            // Act
+            var postResponse = await _testsFixture.Client.PostAsJsonAsync(URICursos, cursoModel);
+            await postResponse.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.Equal(StatusCodes.Status201Created, (int)postResponse.StatusCode);
+        }
+    }
+}
