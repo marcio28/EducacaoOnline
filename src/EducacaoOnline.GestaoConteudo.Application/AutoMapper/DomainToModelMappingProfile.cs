6d8acb8de299fa515c12d69a5142116e402ed881
Marcio <marcio28@gmail.com>
2025-11-10 23:52:15 -0300

    Criado GestaoConteudo.Application.

---
+++ src/EducacaoOnline.GestaoConteudo.Application/AutoMapper/DomainToModelMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using EducacaoOnline.GestaoConteudo.Application.Models;
+using EducacaoOnline.GestaoConteudo.Domain;
+
+namespace EducacaoOnline.GestaoConteudo.Application.AutoMapper
+{
+    public class DomainToModelMappingProfile : Profile
+    {
+        public DomainToModelMappingProfile()
+        {
+            CreateMap<Curso, CursoModel>()
+                .ForMember(d => d.Descricao, o => o.MapFrom(s => s.ConteudoProgramatico.Descricao));
+        }
+    }
+}
---
+++ src/EducacaoOnline.GestaoConteudo.Application/AutoMapper/ModelToDomainMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using EducacaoOnline.GestaoConteudo.Application.Models;
+using EducacaoOnline.GestaoConteudo.Domain;
+
+namespace EducacaoOnline.GestaoConteudo.Application.AutoMapper
+{
+    public class ModelToDomainMappingProfile : Profile
+    {
+        public ModelToDomainMappingProfile()
+        {
+            CreateMap<CursoModel, Curso>()
+                .ForMember(d => d.ConteudoProgramatico, o => o.MapFrom(s => s.Descricao));
+        }
+    }
+}
