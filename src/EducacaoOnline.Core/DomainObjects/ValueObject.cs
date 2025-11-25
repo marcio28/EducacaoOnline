f9164f15ed2a4e57bca51e24b9ebb599b947dde2
Marcio <marcio28@gmail.com>
2025-11-25 00:00:30 -0300

    Propriedades marcadas para não mapeamento.

--- src/EducacaoOnline.Core/DomainObjects/ValueObject.cs
+++ src/EducacaoOnline.Core/DomainObjects/ValueObject.cs
@@ -1,16 +1,21 @@
 ﻿using FluentValidation.Results;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EducacaoOnline.Core.DomainObjects
 {
     public abstract class ValueObject
     {
+        [NotMapped]
         public ValidationResult? ValidationResult { get; protected set; }
+        
+        [NotMapped]
         public List<ValidationFailure> Erros => ValidationResult?.Errors ?? [];
+        
         public int QuantidadeErros => Erros.Count;
 
         public virtual bool EhValido()
         {
-            return false;
+            return true;
         }
     }
 }
