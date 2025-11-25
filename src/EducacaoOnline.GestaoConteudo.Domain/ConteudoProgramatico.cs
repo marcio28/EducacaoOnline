862d5d208a3c2a9fc527a4735cc9d2f6a0d05808
Marcio <marcio28@gmail.com>
2025-11-25 00:02:26 -0300

    Declarado como ValueObject.

--- src/EducacaoOnline.GestaoConteudo.Domain/ConteudoProgramatico.cs
+++ src/EducacaoOnline.GestaoConteudo.Domain/ConteudoProgramatico.cs
@@ -1,7 +1,9 @@
 ﻿
+using EducacaoOnline.Core.DomainObjects;
+
 namespace EducacaoOnline.GestaoConteudo.Domain
 {
-    public class ConteudoProgramatico(string descricao)
+    public class ConteudoProgramatico(string descricao) : ValueObject
     {
         public string Descricao { get; private set; } = descricao;
     }
